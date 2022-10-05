72fe944fda1edf237e33bc2e3af801f49933c7af
MEHDI-svg <mehdiblabo@gmail.com>
2022-10-05 12:44:00 +0100

    container

--- flutter_application_1/lib/lesson_one.dart
+++ flutter_application_1/lib/lesson_one.dart
@@ -5,9 +5,23 @@ class LessonOne extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(title: Text("Fist App Flutter")),
-          body: const Center(
+          appBar: AppBar(
+            backgroundColor: Colors.teal,
+            leading: const Icon(
+              Icons.center_focus_weak_sharp,
+              color: Colors.black,
+            ),
+            title: const Text("Fist App Flutter"),
+            actions: const [
+              Icon(Icons.center_focus_weak_sharp),
+              Icon(Icons.center_focus_weak_sharp),
+              Icon(Icons.center_focus_weak_sharp),
+            ],
+          ),
+          body: Container(
+            color: Colors.blue,
             child: Text("Body"),
           )),
     );
