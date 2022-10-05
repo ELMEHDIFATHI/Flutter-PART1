e40c8cba61181a9abd7ad88f8c199167d330145c
MEHDI-svg <mehdiblabo@gmail.com>
2022-10-05 14:59:08 +0100

    buttonn style pressed iconbutton

--- flutter_application_1/lib/lesson_one.dart
+++ flutter_application_1/lib/lesson_one.dart
@@ -2,6 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LessonOne extends StatelessWidget {
+  const LessonOne({Key? key}) : super(key: key);
+
+  buttonPressed(msg) {
+    print('button clicked ! $msg');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -9,6 +15,7 @@ class LessonOne extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: Colors.teal,
+            //elevation: 0,
             leading: const Icon(
               Icons.center_focus_weak_sharp,
               color: Colors.black,
@@ -21,30 +28,52 @@ class LessonOne extends StatelessWidget {
             ],
           ),
           body: Container(
-              alignment: Alignment.center,
-              color: Colors.blue,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "hello worl",
-                    style: TextStyle(
-                        color: Colors.red,
-                        //backgroundColor: Color.fromARGB(110, 66, 88, 105),
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        wordSpacing: 10,
-                        letterSpacing: 20,
-                        shadows: <Shadow>[
-                          Shadow(
-                              offset: Offset(0.0, 9.0),
-                              blurRadius: 3,
-                              color: Colors.black)
-                        ]),
-                  ),
-                ],
-              ))),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Hello word"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [Text("DATA NOW"), Text("DATA NOW")],
+                ),
+                Row(
+                  children: [
+                    TextButton(onPressed: () {}, child: const Text("A")),
+                    ElevatedButton(onPressed: () {}, child: const Text("B")),
+                    OutlinedButton(onPressed: () {}, child: const Text("C")),
+                  ],
+                ),
+                Row(
+                  children: [
+                    TextButton.icon(
+                      style: ButtonStyle(
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.red),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.black),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(10))),
+                      onPressed: () {},
+                      icon: const Icon(Icons.access_alarm),
+                      label: const Text("A"),
+                      autofocus: true,
+                    ),
+                    ElevatedButton(onPressed: () {}, child: const Text("B")),
+                    OutlinedButton(onPressed: () {}, child: const Text("C")),
+                  ],
+                ),
+                Row(children: [
+                  IconButton(
+                      onPressed: buttonPressed('hello'),
+                      icon: const Icon(Icons.accessibility_new_outlined)),
+                  IconButton(
+                      onPressed: () => print("icon 2 clicked"),
+                      icon: const Icon(Icons.accessibility_new_outlined)),
+                ]),
+              ],
+            ),
+          )),
     );
   }
 }
